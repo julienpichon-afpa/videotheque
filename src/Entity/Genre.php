272275b091080327f23410cf345507f9ad8753ce
Jeremy Kessous <jeremykessous@yahoo.fr>
2022-01-26 15:11:58 +0100

    api endpoints added + fixtures bundle installed + Genre fixture created + genre.json

---
+++ src/Entity/Genre.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\GenreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=GenreRepository::class)
+ */
+class Genre
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
